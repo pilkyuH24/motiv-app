aed7cec7fa7f80fe940ba0c5be0744d55528a876
Pilkyu Han <pil331@berkeley.edu>
2025-03-20 22:37:25 +0900

    eslint error fix 5

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from '@/lib/auth';
 import { prisma } from "../../lib/prisma";
 import { User } from "../user";
 import { LoginButton, LogoutButton } from "../components/auth";
