e6a6b22633d8a1f81d4e42acde94d0441f99bf81
Pilkyu Han <pil331@berkeley.edu>
2025-03-20 22:34:38 +0900

    eslint error fix 4(authOption to lib)

--- app/api/user-missions/route.ts
+++ app/api/user-missions/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from '@/lib/auth';
 import { addDays, addWeeks, addMonths, isBefore } from "date-fns";
 
 // Handles GET requests to retrieve all user missions
